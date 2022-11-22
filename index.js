339c65b0cffadd56076dfe9fd500a27c7e0796eb
IvyW2 <ivywambugha2@gmail.com>
2022-11-22 16:57:16 +0300

    Adding Review: Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Ivy';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
